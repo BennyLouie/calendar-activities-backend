48f7e8b678c80f86b567971482f52f625eec3666
BennyLouie <bennylouie1412@gmail.com>
2022-07-23 12:23:45 -0400

    spring boot app initialized

---
+++ src/main/java/com/benny/practice/calendaractivities/CalendarActivitiesApplication.java
@@ -0,0 +1,13 @@
+package com.benny.practice.calendaractivities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarActivitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarActivitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benny/practice/calendaractivities/CalendarActivitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benny.practice.calendaractivities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarActivitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
