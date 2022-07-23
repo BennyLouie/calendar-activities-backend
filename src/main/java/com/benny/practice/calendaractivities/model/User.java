ff3e2870123bfe63394daf9cac2df85b24c159ef
BennyLouie <bennylouie1412@gmail.com>
2022-07-23 13:58:12 -0400

    added user model

---
+++ src/main/java/com/benny/practice/calendaractivities/model/User.java
@@ -0,0 +1,28 @@
+package com.benny.practice.calendaractivities.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+}
